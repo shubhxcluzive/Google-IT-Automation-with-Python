4d9f4b3a4e5550df2bd6af8bdc4cd765c8eaa180
shubhxluzive <shubhxcluIve@gmail.com>
2020-07-24 18:52:54 +0530

    second module

--- Using-Python-to-Interact-with-the-Operating-System/regular_expressions.py
+++ Using-Python-to-Interact-with-the-Operating-System/regular_expressions.py
@@ -1,3 +1,9 @@
 import re
-result = re.search(r"aza", "plaza")
+result = re.search(r"aza", "plaza") # returns matched string
 print(result)
+print(re.search(r"^x", "xmas"))
+print(re.search(r"p.ng", "Penguin", re.IGNORECASE))
+print(re.search(r"[pP]ython", "Python"))
+print(re.search(r"[a-z]way", "end of highway"))
+print(re.search(r"cloud[a-zA-Z0-9]", "cloud9"))
+print(re.search(r"[^a-zA-Z]", "876 cloud."))  #find string not preceded by character class
